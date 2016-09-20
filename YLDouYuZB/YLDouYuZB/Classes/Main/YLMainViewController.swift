196b131e3b0e217f3c7bab53aa8fbc1bd0ac66e4
pj-Macmini <pj-macmini@pj-macminidemac-mini.local>
2016-09-20 17:14:59 +0800

    设置自控制器

---
+++ YLDouYuZB/YLDouYuZB/Classes/Main/YLMainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  YLMainViewController.swift
+//  YLDouYuZB
+//
+//  Created by pj-Macmini on 16/9/20.
+//  Copyright © 2016年 yl. All rights reserved.
+//
+
+import UIKit
+
+class YLMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChildVC("Home");
+        addChildVC("Live");
+        addChildVC("Follow");
+        addChildVC("Proflie");
+    }
+    
+    private func addChildVC(storyboardName : String){
+        
+        // 1. 通过storyboard获取控制器
+        let childVC = UIStoryboard(name: storyboardName, bundle: nil).instantiateInitialViewController()!;
+        
+        // 2. 将childVC作为自控制器
+        addChildViewController(childVC);
+    }
+
+}
