1b5009fb9ed36c9ba4caa7eade23b90ce5b17e1e
yl <yl@qwedemacbook-pro.local>
2016-09-21 12:55:14 +0800

    设置首页导航栏

--- YLDouYuZB/YLDouYuZB/Classes/Home/Controller/YLHomeViewController.swift
+++ YLDouYuZB/YLDouYuZB/Classes/Home/Controller/YLHomeViewController.swift
@@ -30,32 +30,51 @@ extension YLHomeViewController {
     private func setupNavigationBar(){
         
         // 1.设置左侧的Item
-        let btn = UIButton();
-        btn.setImage(UIImage(named: "logo"), forState: .Normal);
-        btn.sizeToFit();
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn);
+//        let btn = UIButton();
+//        btn.setImage(UIImage(named: "logo"), forState: .Normal);
+//        btn.sizeToFit();
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "logo");
         
         
         // 2.设置左侧的Item
         let size = CGSize(width: 40, height: 40);
         
-        let historyBtn = UIButton();
-        historyBtn.setImage(UIImage(named: "image_my_history"), forState: .Normal);
-        historyBtn.setImage(UIImage(named: "Image_my_history_click"), forState: .Highlighted);
-        historyBtn.frame = CGRect(origin: CGPointZero, size: size);
-        let historyItem = UIBarButtonItem(customView: historyBtn);
+//        let historyBtn = UIButton();
+//        historyBtn.setImage(UIImage(named: "image_my_history"), forState: .Normal);
+//        historyBtn.setImage(UIImage(named: "Image_my_history_click"), forState: .Highlighted);
+//        historyBtn.frame = CGRect(origin: CGPointZero, size: size);
+//        let historyItem = UIBarButtonItem(customView: historyBtn);
+//        
+//        let searchBtn = UIButton();
+//        searchBtn.setImage(UIImage(named: "btn_search"), forState: .Normal);
+//        searchBtn.setImage(UIImage(named: "btn_search_clicked"), forState: .Highlighted);
+//        searchBtn.frame = CGRect(origin: CGPointZero, size: size);
+//        let searchItem = UIBarButtonItem(customView: searchBtn);
+//        
+//        let qrcodeBtn = UIButton();
+//        qrcodeBtn.setImage(UIImage(named: "Image_scan"), forState: .Normal);
+//        qrcodeBtn.setImage(UIImage(named: "Image_scan_click"), forState: .Highlighted);
+//        qrcodeBtn.frame = CGRect(origin: CGPointZero, size: size);
+//        let qrcodeItem = UIBarButtonItem(customView: qrcodeBtn);
         
-        let searchBtn = UIButton();
-        searchBtn.setImage(UIImage(named: "btn_search"), forState: .Normal);
-        searchBtn.setImage(UIImage(named: "btn_search_clicked"), forState: .Highlighted);
-        searchBtn.frame = CGRect(origin: CGPointZero, size: size);
-        let searchItem = UIBarButtonItem(customView: searchBtn);
+        /**
+         *  方法1. 使用类方法创建item(自己构造的类方法)
+         */
+ //        let historyItem = UIBarButtonItem.createItem("image_my_history", highImageName: "Image_my_history_click", size: size);
+//        
+//        let searchItem = UIBarButtonItem.createItem("btn_search", highImageName: "btn_search_clicked", size: size);
+//        
+//        let qrcodeItem = UIBarButtonItem.createItem("Image_scan", highImageName: "Image_scan_click", size: size);
         
-        let qrcodeBtn = UIButton();
-        qrcodeBtn.setImage(UIImage(named: "Image_scan"), forState: .Normal);
-        qrcodeBtn.setImage(UIImage(named: "Image_scan_click"), forState: .Highlighted);
-        qrcodeBtn.frame = CGRect(origin: CGPointZero, size: size);
-        let qrcodeItem = UIBarButtonItem(customView: qrcodeBtn);
+        /**
+         *  方法2. 使用构造函数方法创建item
+         */
+        let historyItem = UIBarButtonItem(imageName: "image_my_history", highImageName: "Image_my_history_click", size: size);
+        
+        let searchItem = UIBarButtonItem(imageName: "btn_search", highImageName: "btn_search_clicked", size: size);
+        
+        let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size);
         
         navigationItem.rightBarButtonItems = [historyItem,searchItem,qrcodeItem];
     }
--- YLDouYuZB/YLDouYuZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
+++ YLDouYuZB/YLDouYuZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 extension UIBarButtonItem{
-    // 1.直接扩展类方法
-//    class func createItem(iamgeName : String, highImageName : String,size : CGSize) -> UIBarButtonItem{
-//        
-//        
+    // 方法1.直接扩展类方法
+//    class func createItem(imageName : String, highImageName : String,size : CGSize) -> UIBarButtonItem{
+//        let btn = UIButton();
+//        btn.setImage(UIImage(named:imageName), forState: .Normal);
+//        btn.setImage(UIImage(named:highImageName), forState: .Highlighted);
+//        btn.frame = CGRect(origin: CGPointZero, size: size);
+//        return UIBarButtonItem(customView: btn);
 //    }
+    
+    // 方法2.用便利构造器方法
+    convenience init(imageName : String, highImageName : String = "",size : CGSize = CGSizeZero) {
+        let btn = UIButton();
+        btn.setImage(UIImage(named: imageName), forState: .Normal);
+        if highImageName != "" {
+            
+            btn.setImage(UIImage(named: highImageName), forState: .Highlighted);
+        }
+        if size == CGSizeZero {
+            btn.sizeToFit();
+        }else {
+            btn.frame = CGRect(origin: CGPointZero, size: size);
+        }
+        self.init(customView:btn);
+    }
 }
